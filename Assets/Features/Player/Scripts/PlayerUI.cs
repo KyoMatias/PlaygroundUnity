d7286a8726cd1639e2f06f95603e9ca9650fef8d
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2026-01-20 12:57:11 +0800

    WIP: PlayerUI, Canvas

---
+++ Assets/Features/Player/Scripts/PlayerUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class PlayerUI : MenuComponents
+{
+    public override void Launch()
+    {
+        Debug.Log("Menu Test Successful");
+    } 
+}
+
+
+// TODO: Create an overall Player UI state.
+// Use Events to call and subscribe to different components.
+
---
+++ Assets/Features/UI/Scripts/MenuComponents.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class MenuComponents : MonoBehaviour
+{
+    public abstract void Launch();
+
+}
